58de69a4c5cea249eddbe57bde9b7dd9dbe78751
Yahya Akermi <yahyaakermi1992@gmail.com>
2020-10-25 11:30:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import ProfilComponent from './Profile/ProfilComponent'
 import picture from './Profile/picture.jpg'
-import Image from './Profile/Image'
 
+const data = {
+  fullName: "Yahya Akermi" ,
+  bio:"Stay humble. Be kind. Work hard.",
+  profession:"I am a web dev",
+  src : picture
+}
 
-function App() {
-  return (
-    <div className="App" style={{backgroundColor:"#d1f593"}}>
-      <ProfilComponent 
-      fullName= "Yahya Akermi" 
-      bio="Stay humble. Be kind. Work hard."
-      profession="I am a web dev"
-      />
-      <Image>{picture}</Image> 
-    </div>
-  );
+class App extends Component {
+
+  handleName= () => {
+    alert (`My Name is ${data.fullName}`);
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <ProfilComponent user={data}>
+          <img src={data.src} style={{height:400, width:250}}/>
+        </ProfilComponent>
+        <button onClick= {this.handleName} style={{border:"1px solid #c816f0", borderRadius:"5px", backGroundColor:"#fa64f0", padding:15, cursor:"pointer"}}> 
+          Click here!
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Profile/ProfilComponent.js
+++ src/Profile/ProfilComponent.js
@@ -1,51 +1,40 @@
-import React, {Component} from 'react'
-import picture from '../Profile/ProfilComponent.js'
-import { toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import React from 'react'
 import PropTypes from 'prop-types';
 
-toast.configure()
 
-class ProfilComponent extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    handleName=()=>{
-                toast('YAHYA AKERMI!')
-            }
-
-    render() {
-        return (
-            <div>
-                <div>
-                    <h1 style={{color:"#5e5075", fontFamily:" Sansita Swashed', cursive;", letterSpacing:"3px"}}>
-                        {this.props.fullName}
-                    </h1>
-                </div>
-                <div>
-                    <h3 style={{fontFamily:"Open Sans", letterSpacing:2, color:"#1628f0"}}> {this.props.bio} </h3>
-                </div>
-                <div>
-                    <button onClick= {this.handleName} style={{border:"1px solid #c816f0", borderRadius:"5px", backGroundColor:"#fa64f0", padding:15, cursor:"pointer"}}> 
-                    {this.props.profession} 
-                    </button>
-                </div>
+const ProfilComponent = (props) => {
+    
+    return (
+        <div>
+            <div style={{margin:10}}>
+                <h1 style={{color:"#5e5075", fontFamily:" Sansita Swashed', cursive;", letterSpacing:"3px"}}>
+                    {props.user.fullName}
+                </h1>
+                <span>{props.children}</span></div>
+            <div style={{margin:10}} >
+                <h3 style={{fontFamily:"Open Sans", letterSpacing:2, color:"#1628f0"}}> {props.user.bio} </h3>
             </div>
-        )
-    }
+            <div style={{margin:10}}>
+                {props.user.profession} 
+            </div>
+        </div>
+    )
 }
 
 export default ProfilComponent;
 
 // Default props
 ProfilComponent.defaultProps = {
-    fullName:"Yahya Akermi" , 
+    fullName:"User Name" , 
+    bio: "Bio",
+    profession: "Profession",
+    src: "./profile/picture.jpg"
 };
 // props types
 ProfilComponent.propTypes = {
-    fullName : PropTypes.string ,
-    bio : PropTypes.string ,
-    profession : PropTypes.string 
+    fullName : PropTypes.string.isRequired ,
+    bio : PropTypes.string.isRequired ,
+    profession : PropTypes.string.isRequired ,
+    src: PropTypes.string.isRequired,
 };
 
